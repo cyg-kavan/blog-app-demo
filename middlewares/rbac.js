4986e84efb0e326d5150be97a145881c09b36951
Kavan Rank <kavan.rank@cygner.in>
2026-02-20 17:19:55 +0530

    feat: add module and permission wise authorization

--- middlewares/rbac.js
+++ middlewares/rbac.js
@@ -1,11 +1,29 @@
-const authorizeRoles = allowedRoles => {
+const permissionData =  require('../database/data.json');
+const authorizeRoles = (module, permission) => {
     return (req, res, next) => {
         if(!req.user) {
             return res.status(401).json({ message: "Unauthorized access" });
         }
 
-        console.log(allowedRoles)
-        if(!allowedRoles.includes(req.user.role)) {
+        console.log(module, permission)
+        // if(!allowedRoles.includes(req.user.role)) {
+        //     return res.status(403).json({ message: "Access denied" });
+        // }
+
+        const userRole = req.user.role;
+        const permissionRole = permissionData[userRole];
+        
+        if(!permissionRole) {
+            return res.status(403).json({ message: "Access denied" });
+        }
+        
+        const permissionModule = permissionRole[module];
+        if(!permissionModule) {
+            return res.status(403).json({ message: "Access denied" });
+        }
+        console.log({userRole, permissionRole, permissionModule});
+
+        if(!permissionModule.includes(permission)) {
             return res.status(403).json({ message: "Access denied" });
         }
 
