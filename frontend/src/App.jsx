f2c0f411c2409f29f4bedf7dc0757ac56d33cb11
Kavan Rank <kavan.rank@cygner.in>
2026-02-25 12:13:26 +0530

    refactor: move Login component to pages and add Signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Login from './components/Login'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/signup" element={<Signup />}></Route>
           <Route path="/login" element={<Login />}></Route>
         </Routes>
       </BrowserRouter>
---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <h1 className='text-amber-300'>Signup</h1>
+  )
+}
+
+export default Signup
