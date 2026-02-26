9aee5fa7053d61862725664f1997adf3619b49c4
Kavan Rank <kavan.rank@cygner.in>
2026-02-26 16:45:07 +0530

    feat: add CORS configuration for frontend integration

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 const userRoutes = require("./routes/user.routes");
 const blogRoutes = require("./routes/blog.routes");
@@ -11,6 +12,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}))
 
 app.use("/api/users", userRoutes);
 app.use("/api/blogs", blogRoutes);
