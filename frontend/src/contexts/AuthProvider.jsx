84a1fdea21d0e802638ace4cabb8f807881bd4e6
Kavan Rank <kavan.rank@cygner.in>
2026-03-06 18:17:08 +0530

    feat(auth): add AuthContext, AuthProvider, useAuth for authentication state management

---
+++ frontend/src/contexts/AuthProvider.jsx
@@ -0,0 +1,42 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { AuthContext } from './AuthContext';
+
+export default function AuthProvider({ children }) {
+
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+        const checkAuthentication = async () => {
+            try {
+                const response = await axios.get("http://localhost:8000/api/users/check-auth", { withCredentials: true });
+                setUser(response.data.user);
+            } catch (error) {
+                console.error("Authentication error", error.response?.data || error.message);
+                setUser(null);
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        useEffect(() => {
+            checkAuthentication();
+        },[])
+
+    // const login = async (credentials) => {
+    //     await axios.post("http://localhost:8000/api/users/login", credentials, { withCredentials: true });
+
+    //     const response = await axios.get(
+    //         "http://localhost:8000/api/users/check-auth",
+    //         { withCredentials: true }
+    //     )
+    
+    //     setUser(response.data.user)
+    // }
+
+    return (
+        <AuthContext.Provider value={{ user, loading, checkAuthentication }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
