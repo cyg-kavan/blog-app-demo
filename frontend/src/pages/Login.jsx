3a855854e0667b22af79340bd41f8916d58468e8
Kavan Rank <kavan.rank@cygner.in>
2026-03-06 18:25:40 +0530

    feat(auth): add Signup and Login functionalities, add Home page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,93 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { useAuth } from '../contexts/useAuth';
+import axios from 'axios';
 
-const Login = () => {
+export default function Login () {
+
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const navigate = useNavigate();
+
+  const { checkAuthentication } = useAuth();
+
+    const handleLogin = async (e) => {
+    e.preventDefault();
+    // try {
+    //   const result = await login({ email, password });
+    //   alert("Login successful")
+    //   navigate("/home")
+    // } catch (error) {
+    //   console.error("Login error", error.message);
+    // }
+
+    try {
+      const response = await axios.post("http://localhost:8000/api/users/login",
+        { email, password },
+        { withCredentials: true }
+      )
+
+      await checkAuthentication();
+
+      alert("Login successful")
+      navigate("/home")
+    } catch (error) {
+      console.error("Login error", error.message);
+    }
+  }
   return (
-    <div>Login</div>
+    <>
+      <div className="flex min-h-screen flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-black">Login to your account</h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form onSubmit={handleLogin} className="space-y-6">
+            <div>
+              <label className="block text-sm/6 font-medium text-black-100">Email address</label>
+              <div className="mt-2">
+                <input
+                  name="email"
+                  type="email"
+                  required
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-black outline-2 -outline-offset-1 outline-black placeholder:text-black focus:outline-2 focus:-outline-offset-2 focus:outline-cyan-500 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label className="block text-sm/6 font-medium text-black-100">Password</label>
+              <div className="mt-2">
+                <input
+                  name="password"
+                  type="password"
+                  required
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-black outline-2 -outline-offset-1 outline-black placeholder:text-black focus:outline-2 focus:-outline-offset-2 focus:outline-cyan-500 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-cyan-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-cyan-400"
+              >
+                Login
+              </button>
+            </div>
+          </form>
+
+          <p className="mt-10 text-center text-sm/6 text-black">
+            Don't have an account?{' '}
+            <Link to="/signup" className="font-semibold text-cyan-400 hover:text-cyan-300">
+              Sign up
+            </Link>
+          </p>
+        </div>
+      </div>
+    </>
   )
 }
-
-export default Login
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,9 +1,104 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from "axios"
+import { useAuth } from '../contexts/useAuth'
 
-const Signup = () => {
+export default function Signup () {
+
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const navigate = useNavigate();
+
+  const { checkAuthentication } = useAuth();
+
+  const handleSignup = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post("http://localhost:8000/api/users/signup",
+        { name, email, password },
+        { withCredentials: true }
+      )
+
+      await checkAuthentication();
+
+      if (response.status === 201) {
+        alert("Signup successful")
+        navigate("/home")
+        // console.log(response.data.user)
+      }
+    } catch (error) {
+      console.error("Signup error", error.response?.data || error.message);
+    }
+  }
+
   return (
-    <h1 className='text-amber-300'>Signup</h1>
+    <>
+      <div className="flex min-h-screen flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-black">Sign up to your account</h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form onSubmit={handleSignup} className="space-y-6">
+            <div>
+              <label className="block text-sm/6 font-medium text-black">Name</label>
+              <div className="mt-2">
+                <input
+                  name="name"
+                  type="text"
+                  required
+                  onChange={(e) => setName(e.target.value)}
+                  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-black outline-2 -outline-offset-1 outline-black placeholder:text-black focus:outline-2 focus:-outline-offset-2 focus:outline-cyan-500 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label className="block text-sm/6 font-medium text-black-100">Email address</label>
+              <div className="mt-2">
+                <input
+                  name="email"
+                  type="email"
+                  required
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-black outline-2 -outline-offset-1 outline-black placeholder:text-black focus:outline-2 focus:-outline-offset-2 focus:outline-cyan-500 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label className="block text-sm/6 font-medium text-black-100">Password</label>
+              <div className="mt-2">
+                <input
+                  name="password"
+                  type="password"
+                  required
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-black outline-2 -outline-offset-1 outline-black placeholder:text-black focus:outline-2 focus:-outline-offset-2 focus:outline-cyan-500 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-cyan-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-cyan-400"
+              >
+                Sign up
+              </button>
+            </div>
+          </form>
+
+          <p className="mt-10 text-center text-sm/6 text-black">
+            Already have an account?{' '}
+            <Link to="/login" className="font-semibold text-cyan-400 hover:text-cyan-300">
+              Login Here
+            </Link>
+          </p>
+        </div>
+      </div>
+    </>
   )
 }
-
-export default Signup
